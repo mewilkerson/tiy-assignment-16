6d951666ecc228d30a37bc583aeb2b10183d4eaa
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-02-24 22:47:56 -0500

    Got the accordian looking somewhat decent

--- js/main.js
+++ js/main.js
@@ -4,8 +4,26 @@ $(function(){
 
     success: function(data) {
       window.data = data;
-      $(".accordian").append( data[1]["paragraph"] );
-      
+      // $(".panel one").append( data[1]["paragraph"] );
+
+      $(".panel-one").click(function(){
+        $(".panel-one p").show(1000);
+        $(".panel-two p").hide();
+        $(".panel-three p").hide();
+        $(".panel-one p").append( data[0]["paragraph"] );
+      });
+        $(".panel-two").click(function(){
+        $(".panel-one p").hide();
+        $(".panel-two p").show(1000);
+        $(".panel-three p").hide();
+        $(".panel-two p").append( data[1]["paragraph"] );
+      });
+        $(".panel-three").click(function(){
+        $(".panel-one p").hide();
+        $(".panel-two p").hide();
+        $(".panel-three p").show(1000);
+        $(".panel-three p").append( data[2]["paragraph"] );
+      });
 
     },
 
